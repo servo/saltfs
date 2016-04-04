0a36db09e677b2d8d90956b29e50aa660ebfdea2
Aneesh Agrawal <aneeshusa@gmail.com>
2016-04-04 12:03:08 -0400

    Add test suite, check executable shebangs
    Start a small test suite in Python 3.
    Add a test to check executable shebangs.

---
+++ test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import importlib
+import os
+import sys
+
+from tests.util import color, GREEN, RED, Failure, project_path
+
+
+def is_python_script(path):
+    return path.endswith('.py')
+
+
+def main():
+    ANY_FAILURES = False
+
+    test_dir = os.path.join(project_path(), 'tests')
+    tests = sorted(filter(is_python_script, os.listdir(test_dir)))
+    for test in tests:
+        test_mod = importlib.import_module('tests.{}'.format(test[:-3]))
+        if not hasattr(test_mod, 'run'):  # Not a test script
+            continue
+
+        try:
+            result = test_mod.run()
+        except Exception as e:
+            result = Failure('Test \'{}\' raised an exception:'.format(test),
+                             str(e))
+
+        if result.is_success():
+            print('[ {} ] {}'.format(color(GREEN, 'PASS'), result.message))
+        else:
+            ANY_FAILURES = True
+            print('[ {} ] {}'.format(color(RED, 'FAIL'), result.message))
+            for line in result.output.splitlines():
+                print('         {}'.format(line))
+
+    return 1 if ANY_FAILURES else 0
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ tests/util.py
@@ -0,0 +1,65 @@
+import os
+
+RED = 31
+GREEN = 32
+BLUE = 34
+MAGENTA = 35
+
+
+def color(code, string):
+    return '\033[' + str(code) + 'm' + string + '\033[0m'
+
+
+def display_path(path):
+    return color(MAGENTA, path)
+
+
+def colon():
+    return color(BLUE, ':')
+
+
+EXCLUDE_DIRS = ['.git', '.vagrant']
+
+
+def project_path():
+    # One dirname for tests dir, another for project dir
+    project_dir = os.path.dirname(os.path.dirname(__file__))
+    common = os.path.commonpath([project_dir, os.getcwd()])
+    return project_dir.replace(common, '.', 1)  # Only replace once
+
+
+def paths():
+    for root, dirs, files in os.walk(project_path(), topdown=True):
+        for exclude_dir in EXCLUDE_DIRS:
+            if exclude_dir in dirs:
+                dirs.remove(exclude_dir)
+
+        for filename in files:
+            yield os.path.join(root, filename)
+
+
+class TestResult(object):
+    pass
+
+
+class Success(TestResult):
+    def __init__(self, message):
+        self.message = message
+
+    def is_success(self):
+        return True
+
+    def is_failure(self):
+        return False
+
+
+class Failure(TestResult):
+    def __init__(self, message, output):
+        self.message = message
+        self.output = output
+
+    def is_success(self):
+        return False
+
+    def is_failure(self):
+        return True
