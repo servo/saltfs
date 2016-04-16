451f3f974da71983c040e322cbf591eb74a640b9
Aneesh Agrawal <aneeshusa@gmail.com>
2016-04-16 14:46:06 -0400

    Create separate file for environments
    The existing mechanism for chaining environment dictionaries is a hack
    that Guido has noted as abuse of the '**' mechanism:
    https://mail.python.org/pipermail/python-dev/2010-April/099459.html
    
    Create a subclass that allows 'adding' in a single expression, as
    copy() + update() takes two lines.
    
    Use str.join for listing PATHs to keep lines short. This also exposed
    an error in the Windows PATH, with a missing semicolon between
    r'C:\msys64\usr\bin' and r'C:\Windows\system32'.
    
    Move the environments into a separate file to keep them all together.
    Rename the various environment variables for clarity and consistency.
    
    The environments.py file now passes flake8; this should be added to our
    test suite.

---
+++ buildbot/master/files/config/environments.py
@@ -0,0 +1,119 @@
+from passwords import GITHUB_DOC_TOKEN
+
+
+class Environment(dict):
+    """
+    Wrapper that allows 'adding' environment dictionaries
+    to make it easy to build up environments piece by piece.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(Environment, self).__init__(*args, **kwargs)
+
+    def __add__(self, other):
+        assert type(self) == type(other)
+        combined = self.copy()
+        combined.update(other)  # other takes precedence over self
+        return Environment(combined)
+
+
+doc = Environment({
+    'CARGO_HOME': '{{ common.servo_home }}/.cargo',
+    'SERVO_CACHE_DIR': '{{ common.servo_home }}/.servo',
+    'SHELL': '/bin/bash',
+    'TOKEN': GITHUB_DOC_TOKEN,
+})
+
+build_common = Environment({
+    'RUST_BACKTRACE': '1',
+})
+
+build_windows = build_common + Environment({
+    'MSYS': 'winsymlinks=lnk',
+    'MSYSTEM': 'MINGW64',
+    'PATH': ';'.join([
+        r'C:\msys64\mingw64\bin',
+        r'C:\msys64\usr\bin',
+        r'C:\Windows\system32',
+        r'C:\Windows',
+        r'C:\Windows\System32\Wbem',
+        r'C:\Windows\System32\WindowsPowerShell\v1.0',
+        r'C:\Program Files\Amazon\cfn-bootstrap',
+    ]),
+})
+
+build_mac = build_common + Environment({
+    'CARGO_HOME': '/Users/servo/.cargo',
+    'CCACHE': '/usr/local/bin/ccache',
+    'SERVO_CACHE_DIR': '/Users/servo/.servo',
+})
+
+
+build_linux = build_common + Environment({
+    'CARGO_HOME': '{{ common.servo_home }}/.cargo',
+    'CCACHE': '/usr/bin/ccache',
+    'DISPLAY': ':0',
+    'SERVO_CACHE_DIR': '{{ common.servo_home }}/.servo',
+    'SHELL': '/bin/bash',
+})
+
+build_linux_headless = build_linux + Environment({
+    'SERVO_HEADLESS': '1',
+})
+
+build_android = build_linux + Environment({
+    'ANDROID_NDK': '{{ common.servo_home }}/android/ndk/current/',
+    'ANDROID_SDK': '{{ common.servo_home }}/android/sdk/current/',
+    'ANDROID_TOOLCHAIN': '{{ common.servo_home }}/android/toolchain/current/',
+    'PATH': ':'.join([
+        '/usr/local/sbin',
+        '/usr/local/bin',
+        '/usr/bin',
+        '/usr/sbin',
+        '/sbin',
+        '/bin',
+        '{{ common.servo_home }}/android/sdk/current/platform-tools',
+        '{{ common.servo_home }}/android/toolchain/current/bin',
+    ]),
+})
+
+build_arm = build_linux + Environment({
+    'EXPAT_NO_PKG_CONFIG': '1',
+    'FONTCONFIG_NO_PKG_CONFIG': '1',
+    'FREETYPE2_NO_PKG_CONFIG': '1',
+    'PKG_CONFIG_ALLOW_CROSS': '1',
+})
+
+
+build_arm32 = build_arm + Environment({
+    'BUILD_TARGET': 'arm-unknown-linux-gnueabihf',
+    'CC': 'arm-linux-gnueabihf-gcc',
+    'CXX': 'arm-linux-gnueabihf-g++',
+    'PATH': ':'.join([
+        '{{ common.servo_home }}/bin',
+        '/usr/local/sbin',
+        '/usr/local/bin',
+        '/usr/bin',
+        '/usr/sbin',
+        '/sbin',
+        '/bin',
+    ]),
+    'PKG_CONFIG_PATH': '/usr/lib/arm-linux-gnueabihf/pkgconfig',
+})
+
+build_arm64 = build_arm + Environment({
+    'BUILD_TARGET': 'aarch64-unknown-linux-gnu',
+    'CC': 'aarch64-linux-gnu-gcc',
+    'CXX': 'aarch64-linux-gnu-g++',
+    'PATH': ':'.join([
+        '{{ common.servo_home }}/bin',
+        '/usr/local/sbin',
+        '/usr/local/bin',
+        '/usr/bin',
+        '/usr/sbin',
+        '/sbin',
+        '/bin',
+    ]),
+    'PKG_CONFIG_PATH': '/usr/lib/aarch64-linux-gnu/pkgconfig',
+    'SERVO_RUSTC_WITH_GOLD': 'False',
+})
