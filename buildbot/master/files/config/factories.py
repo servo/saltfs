31aea40748adf969862e5f77152f3c81c4c2e4aa
Aneesh Agrawal <aneeshusa@gmail.com>
2016-04-18 19:39:09 -0400

    Re-add pkill step
    This automatically runs pkill for each build. It is cleaner to put this
    in the main configuration instead of the lists of steps because this is
    a CI-related cleanup task, not a necessary step to run the build + test
    suite. Also, this ensures that the pkill step cannot be forgotten when
    adding new builders.
    
    Note: this doesn't work on Windows yet.

--- buildbot/master/files/config/factories.py
+++ buildbot/master/files/config/factories.py
@@ -2,6 +2,7 @@ import re
 
 from buildbot.plugins import steps, util
 from buildbot.process import buildstep
+from buildbot.status.results import SUCCESS
 
 import environments as envs
 
@@ -58,9 +59,13 @@ class DynamicServoFactory(ServoFactory):
             print(str(e))
             steps = [BadConfigurationStep(e)]
 
+        # TODO: windows compatibility (use a custom script for this?)
+        pkill_step = [steps.ShellCommand(command=["pkill", "-x", "servo"],
+                                        decodeRC={0: SUCCESS, 1: SUCCESS})]
+
         # util.BuildFactory is an old-style class so we cannot use super()
         # but must hardcode the superclass here
-        ServoFactory.__init__(self, steps)
+        ServoFactory.__init__(self, pkill_step + steps)
 
     def make_step(self, command):
         step_kwargs = {}
