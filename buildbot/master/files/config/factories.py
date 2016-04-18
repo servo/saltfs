cf3adb1f07d18db405d81830e9d6692d2cdf3a04
Aneesh Agrawal <aneeshusa@gmail.com>
2016-04-18 03:34:13 -0400

    Use a YAML file to drive dynamic builders
    As a stepping-stone to having builder steps live in the main Servo repo,
    switch to a simple data format to express builder configuration (lists
    of strings accessed via yaml.safe_load) instead of needing to run or
    import arbitrary untrusted Python code.
    
    Currently, the YAML file is still part of the master configuration, but
    it is now ready to be moved to the main Servo repo.

--- buildbot/master/files/config/factories.py
+++ buildbot/master/files/config/factories.py
@@ -1,8 +1,10 @@
+import os.path
 import re
 
 from buildbot.plugins import steps, util
 from buildbot.process import buildstep
 from buildbot.status.results import SUCCESS
+import yaml
 
 import environments as envs
 
@@ -46,14 +48,19 @@ class BadConfigurationStep(buildstep.BuildStep):
 
 class DynamicServoFactory(ServoFactory):
     """
-    Smart factory which takes a list of shell commands and creates the
-    appropriate Buildbot Steps. Uses heuristics to infer Step type, if
-    there are any logfiles, etc.
+    Smart factory which takes a list of shell commands from a yaml file
+    and creates the appropriate Buildbot Steps. Uses heuristics to infer
+    Step type, if there are any logfiles, etc.
     """
 
-    def __init__(self, environment, commands):
+    def __init__(self, builder_name, environment):
         self.environment = environment
         try:
+            config_dir = os.path.dirname(os.path.realpath(__file__))
+            yaml_path = os.path.join(config_dir, 'steps.yml')
+            with open(yaml_path) as steps_file:
+                builder_steps = yaml.safe_load(steps_file)
+            commands = builder_steps[builder_name]
             dynamic_steps = [self.make_step(command) for command in commands]
         except Exception as e:  # Bad step configuration, fail build
             print(str(e))
@@ -115,78 +122,4 @@ windows = ServoFactory([
                   env=envs.build_windows),
     steps.Compile(command=["bash", "-l", "-c", "./mach test-unit"],
                   env=envs.build_windows),
-])
-
-mac_rel_wpt = DynamicServoFactory(envs.build_mac, [
-    "./mach test-tidy --no-progress",
-    "./mach build --release",
-    "./mach test-wpt-failure",
-    "./mach test-wpt --release --processes 4 --log-raw test-wpt.log --log-errorsummary wpt-errorsummary.log",
-    "./mach build-cef --release",
-    "bash ./etc/ci/lockfile_changed.sh",
-    "bash ./etc/ci/manifest_changed.sh",
-])
-
-mac_dev_unit = DynamicServoFactory(envs.build_mac, [
-    "./mach build --dev",
-    "./mach test-unit",
-    "./mach build-cef",
-    "./mach build-geckolib",
-    "bash ./etc/ci/lockfile_changed.sh",
-    "bash ./etc/ci/manifest_changed.sh",
-])
-
-mac_rel_css = DynamicServoFactory(envs.build_mac, [
-    "./mach build --release",
-    "./mach test-css --release --processes 4 --log-raw test-css.log --log-errorsummary css-errorsummary.log",
-    "./mach build-geckolib --release",
-    "bash ./etc/ci/lockfile_changed.sh",
-    "bash ./etc/ci/manifest_changed.sh",
-])
-
-linux_dev = DynamicServoFactory(envs.build_linux_headless, [
-    "./mach test-tidy --no-progress",
-    "./mach test-tidy --no-progress --self-test",
-    "./mach build --dev",
-    "./mach test-compiletest",
-    "./mach test-unit",
-    "./mach build-cef",
-    "./mach build-geckolib",
-    "bash ./etc/ci/lockfile_changed.sh",
-    "bash ./etc/ci/manifest_changed.sh",
-])
-
-linux_rel = DynamicServoFactory(envs.build_linux_headless, [
-    "./mach build --release",
-    "./mach test-wpt-failure",
-    "./mach test-wpt --release --processes 24 --log-raw test-wpt.log --log-errorsummary wpt-errorsummary.log",
-    "./mach test-css --release --processes 16 --log-raw test-css.log --log-errorsummary css-errorsummary.log",
-    "./mach build-cef --release",
-    "./mach build-geckolib --release",
-    "bash ./etc/ci/lockfile_changed.sh",
-    "bash ./etc/ci/manifest_changed.sh",
-    "bash ./etc/ci/check_no_unwrap.sh",
-])
-
-android = DynamicServoFactory(envs.build_android, [
-    "./mach build --android --dev",
-    "bash ./etc/ci/lockfile_changed.sh",
-    "bash ./etc/ci/manifest_changed.sh",
-    "python ./etc/ci/check_dynamic_symbols.py",
-])
-
-android_nightly = DynamicServoFactory(envs.build_android, [
-    "./mach build --android --release",
-    "./mach package -r",
-    ("s3cmd put "
-     "./target/arm-linux-androideabi/release/servo.apk "
-     "s3://servo-rust/nightly/servo.apk"),
-])
-
-arm32 = DynamicServoFactory(envs.build_arm32, [
-    "./mach build --rel --target=arm-unknown-linux-gnueabihf",
-])
-
-arm64 = DynamicServoFactory(envs.build_arm64, [
-    "./mach build --rel --target=aarch64-unknown-linux-gnu",
 ])
