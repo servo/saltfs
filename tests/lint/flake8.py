acf76a1b9625895654145654ea8b5a02ac9c6903
Aneesh Agrawal <aneeshusa@gmail.com>
2016-04-18 21:00:55 -0400

    Add flake8 lint test for other python files
    Currently, the only other python files are the test suite.

---
+++ tests/lint/flake8.py
@@ -0,0 +1,19 @@
+import os
+import subprocess
+
+from tests.util import Failure, Success, project_path
+
+
+def run():
+    paths = ['test.py', 'tests']
+    paths = [os.path.join(project_path(), path) for path in paths]
+    command = ['flake8'] + paths
+    ret = subprocess.run(command,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE,
+                         universal_newlines=True)
+
+    if ret.returncode == 0:
+        return Success("Tests passed flake8 lint")
+    else:
+        return Failure("Tests failed flake8 lint:", ret.stdout)
