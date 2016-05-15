c66f555aa8aa2b4de3a4d923ceeb27dcb328991c
Aneesh Agrawal <aneeshusa@gmail.com>
2016-05-15 01:53:27 -0400

    Also test Salt config changes as an upgrade
    The current testing always starts from scratch.
    However, in practice we do not rebuild all our machines from scratch
    on every change, but instead usually incrementally upgrade them.
    
    Run the tests both from scratch, which is used whenever we stand up
    a new builder, and as an upgrade from the previous configuration, which
    is the more common case.
    
    Additionally, the Salt installation and Salt grains.items and
    state.show_highstate commands produce output that is useful for
    debugging, but is normally not necessary to view.
    Add special markers to the build log that fold these sections on Travis.
    This is especially useful with the longer build logs.

--- .travis/setup_salt_roots.sh
+++ .travis/setup_salt_roots.sh
@@ -4,6 +4,7 @@ set -o errexit
 set -o nounset
 
 setup_salt_roots () {
+    sudo rm -rf /srv/salt
     sudo mkdir -p /srv/salt
     sudo cp -r . /srv/salt/states
     sudo cp -r .travis/test_pillars /srv/salt/pillars
