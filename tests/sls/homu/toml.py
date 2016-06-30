75b59d8c24578dae642ac51c79cf9e7d23b4e405
Christopher McKee <cmckee41@qub.ac.uk>
2016-06-30 21:04:52 +0100

    Initial attempt to solve issue 407

---
+++ tests/sls/homu/toml.py
@@ -0,0 +1,15 @@
+import os
+
+import toml
+
+from tests.util import Failure, Success, project_path
+
+
+def run():
+    config_path = os.path.join(project_path(), 'homu', 'files', 'cfg.toml')
+    with open(config_path) as conf:
+        try:
+            toml.loads(conf.read())
+            return Success('Homu config file is valid TOML')
+        except Exception as e:
+            return Failure('Homu config file is not valid TOML: ', '{}'.format(e))
