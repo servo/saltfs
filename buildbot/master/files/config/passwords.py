f9f12304ad3bb393812c825c9c0df667f38e544a
Aneesh Agrawal <aneeshusa@gmail.com>
2016-06-29 21:53:48 -0400

    Add Linux and Mac nightlies
    Uses new etc/ci/upload_nightly.sh script.
    Passes credentials more securely via environment variables.

--- buildbot/master/files/config/passwords.py
+++ buildbot/master/files/config/passwords.py
@@ -1,6 +1,18 @@
-HTTP_USERNAME = "{{ pillar['buildbot']['credentials']['http-user'] }}"
-HTTP_PASSWORD = "{{ pillar['buildbot']['credentials']['http-pass'] }}"
-SLAVE_PASSWORD = "{{ pillar['buildbot']['credentials']['slave-pass'] }}"
-CHANGE_PASSWORD = "{{ pillar['buildbot']['credentials']['change-pass'] }}"
-GITHUB_DOC_TOKEN = "{{pillar['buildbot']['credentials']['gh-doc-token'] }}"
-HOMU_BUILDBOT_SECRET = "{{pillar['buildbot']['credentials']['homu-secret'] }}"
+import json
+
+# Jinja will replace the inside with double-quote-using JSON,
+# so use single quotes to delimit the string.
+# Use double quotes inside to keep the expression as a single string.
+credentials = json.loads('{{ pillar["buildbot"]["credentials"]|json }}')
+# json.loads creates unicode strings but Buildbot requires bytestrings.
+# Python 2's Unicode situation makes me sad.
+credentials = {k: v.encode('utf-8') for k, v in credentials.items()}
+
+HTTP_USERNAME = credentials['http-user']
+HTTP_PASSWORD = credentials['http-pass']
+SLAVE_PASSWORD = credentials['slave-pass']
+CHANGE_PASSWORD = credentials['change-pass']
+GITHUB_DOC_TOKEN = credentials['gh-doc-token']
+HOMU_BUILDBOT_SECRET = credentials['homu-secret']
+S3_UPLOAD_ACCESS_KEY_ID = credentials['s3-upload-access-key-id']
+S3_UPLOAD_SECRET_ACCESS_KEY = credentials['s3-upload-secret-access-key']
