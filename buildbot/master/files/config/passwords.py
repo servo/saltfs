d76de1d6047a0e2234bd190af1de2e4197d75bfe
Paul Rouget <me@paulrouget.com>
2016-08-16 06:20:27 +0200

    Add brew scripts to nightly

--- buildbot/master/files/config/passwords.py
+++ buildbot/master/files/config/passwords.py
@@ -3,6 +3,7 @@ HTTP_PASSWORD = "{{ buildbot_credentials['http-pass'] }}"
 SLAVE_PASSWORD = "{{ buildbot_credentials['slave-pass'] }}"
 CHANGE_PASSWORD = "{{ buildbot_credentials['change-pass'] }}"
 GITHUB_DOC_TOKEN = "{{ buildbot_credentials['gh-doc-token'] }}"
+GITHUB_HOMEBREW_TOKEN = "{{ buildbot_credentials['gh-homebrew-token'] }}"
 HOMU_BUILDBOT_SECRET = "{{ buildbot_credentials['homu-secret'] }}"
 S3_UPLOAD_ACCESS_KEY_ID = \
     "{{ buildbot_credentials['s3-upload-access-key-id'] }}"
