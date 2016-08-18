143c3f12c1eeb375ee5b55b0665bb7d2b995f355
Paul Rouget <me@paulrouget.com>
2016-08-18 10:18:06 +0200

    Set token for homebrew repository

--- buildbot/master/files/config/factories.py
+++ buildbot/master/files/config/factories.py
@@ -132,6 +132,11 @@ class DynamicServoFactory(ServoFactory):
                         r'C:\Program Files\Amazon\cfn-bootstrap',
                     ])
 
+            # Set token for homebrew repository
+            elif arg == './etc/ci/update_brew.sh'
+                step_kwargs['logEnviron'] = False
+                step_env += envs.update_brew
+
             else:
                 step_desc += [arg]
 
