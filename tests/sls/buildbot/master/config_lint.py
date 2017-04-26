5fc868a0f5916a273fc20aa6095d2c793bec162b
Aneesh Agrawal <aneeshusa@gmail.com>
2017-04-26 15:01:55 -0400

    Drop Python requirement for tests to 3.4 from 3.5
    Ubuntu Trusty's Python 3 package supplies 3.4,
    so this makes it possibly to run the tests on Trusty machines
    without manually installing Python 3.5.
    
    To allow running inside Docker,
    create our own virtualenv for Python test dependencies (e.g. `jinja2`).
    Don't use the virtualenvs supplied by Travis.
    Make sure to install some extra Python bits
    that aren't installed by default for Docker.
    
    Rewrite testing code to avoid APIs introduced in Python 3.5.
    Use block indent and early return in tests.

--- tests/sls/buildbot/master/config_lint.py
+++ tests/sls/buildbot/master/config_lint.py
@@ -5,16 +5,24 @@ from tests.util import Failure, Success, project_path
 
 
 def run():
-    CONF_DIR = os.path.join(project_path(),
-                            'buildbot', 'master', 'files', 'config')
+    CONF_DIR = os.path.join(
+        project_path(),
+        'buildbot',
+        'master',
+        'files',
+        'config'
+    )
     # Have to specify master.cfg separately because it is not a .py file
     command = ['flake8', CONF_DIR, os.path.join(CONF_DIR, 'master.cfg')]
-    ret = subprocess.run(command,
-                         stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE,
-                         universal_newlines=True)
+    proc = subprocess.Popen(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True
+    )
+    stdout, _ = proc.communicate()
 
-    if ret.returncode == 0:
-        return Success("Buildbot master config passed linting")
-    else:
-        return Failure("Buildbot master config lint check failed:", ret.stdout)
+    if proc.returncode != 0:
+        return Failure("Buildbot master config lint check failed:", stdout)
+
+    return Success("Buildbot master config passed linting")
