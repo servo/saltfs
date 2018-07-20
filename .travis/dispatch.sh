cc72071ae805233dbeb794ef4a8c055317689f83
Josh Matthews <josh@joshmatthews.net>
2018-07-20 13:05:00 -0400

    upgrade more ssl

--- .travis/dispatch.sh
+++ .travis/dispatch.sh
@@ -100,20 +100,25 @@ if [[ "${SALT_NODE_ID}" =~ servo-mac.* ]]; then
     travis_fold_start 'update_homebrew' 'Updating homebrew'
     brew update
     travis_fold_end 'update_homebrew'
+
     travis_fold_start 'upgrade_python' 'Upgrading python2->python3'
     brew upgrade python
     travis_fold_end 'upgrade_python'
+
     travis_fold_start 'remove_python' 'Removing python'
     brew uninstall python --ignore-dependencies --force
     travis_fold_end 'remove_python'
-    travis_fold_start 'upgrade_ssl' 'Upgrading openssl'
-    brew upgrade openssl
-    travis_fold_end 'upgrade_ssl'
-    export PATH="/usr/local/Cellar/saltstack/2016.3.3/libexec/bin:$PATH"
-    #travis_fold_start 'install_python2' 'Installing python2'
-    #brew install python@2
+
+    travis_fold_start 'install_python2' 'Installing python2'
+    brew install python@2
     #pip install --upgrade pip setuptools
-    #travis_fold_end 'install_python2'
+    travis_fold_end 'install_python2'
+
+    travis_fold_start 'upgrade_ssl' 'Upgrading openssl'
+    brew upgrade openssl || brew install openssl || true
+    pip install pyOpenSSL
+    travis_fold_end 'upgrade_ssl'
+    #export PATH="/usr/local/Cellar/saltstack/2016.3.3/libexec/bin:$PATH"
 fi
 
 if [[ "${SALT_NODE_ID}" == "test" ]]; then
